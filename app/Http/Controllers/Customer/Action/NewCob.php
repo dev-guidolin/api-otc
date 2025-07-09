3b711f2507b06f37268143f97fe01a7489e009bd
turistacode <developer.arruda@gmail.com>
2025-07-09 07:24:47 -0300

    Cors

--- app/Http/Controllers/Customer/Action/NewCob.php
+++ app/Http/Controllers/Customer/Action/NewCob.php
@@ -30,15 +30,14 @@ class NewCob
 
 
         return $response->json();*/
-        $deposit = CustomerDeposit::query()->create([
+        return CustomerDeposit::query()->create([
             'transaction_id' => Generator::externalId(),
             'user_id' => auth()->id(),
             'amount' => $amount,
             'emv' => '5A0847617390012345675F24032312315F340101820258008407A0000000031010950500000000009A031231239C01009F02060000000010009F03060000000000009F0702FF009F080200029F0D05B0B0B0B0B09F0E0500000000009F0F05B0B0B0B0B09F10120110A00003220000000000000000000000FF9F1101019F1A0209869F1E0831323334353637389F2608AABBCCDDEEFF11229F2701809F3303E0F0C89F34034203009F3501229F3602001E9F3704A1B2C3D49F410400000001',
-            'status' => StatusEnum::Created->value,
+            'status' => StatusEnum::Paid->value,
             'expire_at' => now()->addMinutes(30),
         ]);
 
-        return $deposit;
     }
 }
